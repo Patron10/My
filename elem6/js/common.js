187833ff13014cb332e560f44cae941203813c7d
Andrey Petruk <ildarbogunov@gmail.com>
2016-12-23 16:54:45 +0200

    elem6

--- elem6/js/common.js
+++ elem6/js/common.js
@@ -1,14 +1,22 @@
-// jquery resize height
 $(document).ready(function() {
+    
+    $(".popup").magnificPopup({type:"image"});
+    $(".top_text h1").animated("fadeInDown", "fadeOutUp");
+    $(".top_text p, .section_header").animated("fadeInUp", "fadeOutDown");
+    
+    $(".animation").animated("flipInY", "flipOutY");
+    $(".animatio").animated("fadeInLeft", "fadeOutLeft");
+    $(".animati").animated("fadeInRight", "fadeOutRight");
+    
     function heightDetect(){
       $(".main_head").css("height", $(window).height());  
     };
-    heightDetect();
+        heightDetect();
     $(window).resize(function() {
         heightDetect();
     });
 });
-//
+
 $(".toggle_mnu, .menu_item").click(function() {
     $(".sandwich").toggleClass("active");
 });
