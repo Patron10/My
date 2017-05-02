b729d3e9d5530def8dd15fc882908606e46cdf72
Andrey Petruk <ildarbogunov@gmail.com>
2017-05-02 12:56:24 +0300

    dont

--- js/dz-11-12/Plagin/js/script.js
+++
@@ -1,3 +0,0 @@
-$(function() {
-    $('.carusel').carusel();
-})
--- js/dz_9/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-			/* select style */
-			var params = {
-				changedEl: "select",
-				visRows: 5,
-				scrollArrows: true
-			}
-			cuSel(params);
-		});
